c97e8a6432f3f6b50cb3bf80b39ee8c25b783396
keshe4ka <artem.anaschenko@gmail.com>
2021-09-02 02:43:10 +0300

    изменены параметры

--- values.py
+++ values.py
@@ -3,10 +3,10 @@ import os
 # get cpu temperature
 temperature = os.popen('vcgencmd measure_temp').readline()
 # get Disk Space used %
-disk_space = os.popen("df -h /").read()
+# disk_space = os.popen("df -h /").read()
 # get RAM info
-ram_info = os.popen('free').read()
+# ram_info = os.popen('free').read()
 # get uptime and cpu load average 1 min, 5min and 15 min
 uptime = os.popen('uptime').readline()
 
-info = temperature + '\n' + disk_space + '\n' + ram_info + '\n' + uptime
+info = temperature + '\n' + uptime
